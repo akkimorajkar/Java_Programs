a7536412314cabe884a8a037f5c2c57dd6b5d4c3
akkimorajkar <morajkarakshay@gmail.com>
2020-12-08 17:17:48 +0530

    6th Commit

---
+++ Conditional_Operator.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Conditional_Operator {
+
+    public static void main (String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String Akshay_marks = br.readLine();
+        int AkshayMarks = Integer.parseInt(Akshay_marks);
+
+        String Sushil_marks = br.readLine();
+        int SushilMarks = Integer.parseInt(Sushil_marks);
+
+        int Higest_Scorrer = (AkshayMarks>SushilMarks)? AkshayMarks : SushilMarks;
+
+        System.out.println("The Highest Score is "+ Higest_Scorrer);
+
+
+
+    }
+}
