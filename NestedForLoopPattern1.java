e4259fe3cfc77d0331e0486d073f3b2e2483408c
akkimorajkar <morajkarakshay@gmail.com>
2020-12-09 00:43:14 +0530

    8th Commit

--- NestedForLoopPattern1.java
+++ NestedForLoopPattern1.java
@@ -34,24 +34,7 @@ public class NestedForLoopPattern1 {
             System.out.println();
         }
 
-        /*for (int i = 1; i <= n/2 ; i++){
-
-            for (int j = 1 ; j <= i ; j++){
-
-                System.out.print(" ");
-            }
-
-            for (int k = 1 ; k<=n/2-i ; k++){
-
-                System.out.print("* ");
-
-            }
-
-            System.out.println();
-        } */
-
-
-
+        // Removal of Commented For Loop
 
     }
 
