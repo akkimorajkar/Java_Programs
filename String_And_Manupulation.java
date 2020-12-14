fcb0860b7358d7412513078f39c235d75b9965c0
akkimorajkar <morajkarakshay@gmail.com>
2020-12-14 14:37:09 +0530

    15th Commit

--- String_And_Manupulation.java
+++ String_And_Manupulation.java
@@ -18,7 +18,7 @@ public class String_And_Manupulation {
 
         System.out.println(str.indexOf("t",3)); // Giving the index value > then the first occurence of t'this metod is case sensitive
 
-        System.out.println(str.indexOf("s", str.indexOf("s")+1));
+        System.out.println(str.indexOf("s", str.indexOf("s")+1)); // 2nd Ocurrence of s
 
         Scanner sc = new Scanner(System.in);
 
@@ -30,11 +30,15 @@ public class String_And_Manupulation {
 
         if(str2.indexOf(username)> 0){
 
-            System.out.println("The user ");
+            System.out.println(str2);
 
-            System.out.println("This is new Branch created Locally named as Brnach_Locally ");
+            //System.out.println("This is new Branch created Locally named as Brnach_Locally ");
 
         }
+
+        String str3 = "   Hello World   ";
+
+        System.out.println(str3.trim());
 
     }
 }
